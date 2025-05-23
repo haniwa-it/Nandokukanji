f908d014798206bc4a54641caf673a34902e9fbf
haniwa-it <develop.for.management.site.2024@gmail.com>
2025-05-23 19:53:38 +0900

    Update main_js.js

--- main_js.js
+++ main_js.js
@@ -1,23 +1,21 @@
-// HTML側で定義されたグローバル変数を使用
 const keyword = window.keyword || '';
 
 const requesturl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTbGxBlSXApgZNxfA4UlXJt-3ZEk3eRWaTLdxnsF9UDMP3EKrqC5nD7-T0fromp29n2-qvMOr7T8NhK/pub';
 
 async function fetchAndProcessCSV() {
-  const response = await fetch(requesturl + '?output=csv');
-  const csvText = await response.text();
+    const response = await fetch(requesturl + '?output=csv');
+    const csvText = await response.text();
 
-  const rows = csvText.trim().split('\n').map(row => row.split(','));
+    const rows = csvText.trim().split('\n').map(row => row.split(','));
+    let filteredRows = rows.filter(row => row[0].includes(keyword));
 
-  let filteredRows = rows.filter(row => row[0].includes(keyword));
+    let allhtml = [];
 
-  let allhtml = [];
-
-  for (let i = 0; i < filteredRows.length; i++) {
-    const row = filteredRows[i];
-    const kanji = row[1] || '';
-    const yomi = row[2] || '';
-    const rowhtml = `
+    for (let i = 0; i < filteredRows.length; i++) {
+        const row = filteredRows[i];
+        const kanji = row[1] || '';
+        const yomi = row[2] || '';
+        const rowhtml = `
       <h3>${kanji}</h3>
       <p>${yomi}</p>
       <div class='search'>
@@ -29,9 +27,28 @@ async function fetchAndProcessCSV() {
         </ul>
       </div>
     `;
-    allhtml.push(rowhtml);
-  }
+        allhtml.push(rowhtml);
+    }
 
-  document.getElementById('container').innerHTML = allhtml.join('\n');
+    document.getElementById('container').innerHTML = allhtml.join('\n');
 }
 
+function setHeader() {
+    document.getElementById('header').innerHTML = `<h2>${keyword}</h2>`;
+}
+
+function setFooter() {
+    document.getElementById('footer').innerHTML = `
+          <p>
+            当サイトでは、Google アナリティクスを使用して、ユーザーのサイト利用状況を分析しています。これにより、サイトの改善に役立てるためのデータ収集を行っています。Google アナリティクスが収集する情報には、閲覧ページ、滞在時間、デバイス情報などが含まれますが、個人を特定する情報は含まれていません。
+          </p>
+          <p>
+            詳細については、<a href="https://www.google.com/analytics/terms/jp.html" target="_blank">Google アナリティクスのプライバシーポリシー</a>をご確認ください。
+          </p>
+        `;
+}
+document.addEventListener("DOMContentLoaded", () => {
+    setHeader();
+    setFooter();
+    fetchAndProcessCSV();
+});
